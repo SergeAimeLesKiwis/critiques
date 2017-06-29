9bc141e2544401be1c46ef549a4f13dad33131d6
Yoann Flégeau <yoann.flegeau@gmail.com>
2017-06-29 22:39:56 +0200

    ouh watch me watch me ouh ouh ouh

--- application/views/shared/_item_container.php
+++ application/views/shared/_item_container.php
@@ -1,4 +1,4 @@
-<div class="col-md-4 portfolio-item thumbnail">
+<div class="col-md-4 portfolio-item">
 	<?php echo $item->getImageTag(); ?>
 	<h3>
 		<a href="<?php echo 'room/'.$item->title;?>" class="text-center"><?php echo $item->title; ?></a>
