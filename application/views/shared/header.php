20a61af62f0b31bc9ea213c06bfcb40a46b73696
Yoann Flégeau <yoann.flegeau@gmail.com>
2017-07-10 12:46:33 +0200

    Adding mdbootstrap

--- application/views/shared/header.php
+++ application/views/shared/header.php
@@ -2,17 +2,14 @@
 <html>
 	<head>
 		<title><?php echo $title; ?></title>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
-<!-- BOOTSTRAP -->
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap.css" />
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" />
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/mdb.min.css" />
 		<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/bootstrap.js"></script>
 		
-<!-- SITE -->
 		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/styles/site.css" />
 		<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/scripts/site.js"></script>
 
-<!-- SPECIFIC -->
 		<?php foreach ($links as $link) { echo $link; } ?>
 	</head>
 
